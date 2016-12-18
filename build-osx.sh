516e0345deb2356cfa29fb08596741156ed2253b
leico <yoshitaka_12@iamas.ac.jp>
2016-12-18 22:42:52 +0900

    change config path

--- build-osx.sh
+++ build-osx.sh
@@ -1,6 +1,6 @@
 cd "${SRCROOT}/openssl"
 
-./config $OPENSSL_XCODE_CONFIGURE_ARGS
+./config "$OPENSSL_XCODE_CONFIGURE_ARGS"
 makeOptions="CFLAG=\"-D_DARWIN_C_SOURCE $archFlag $isysroot\" SHARED_LDFLAGS=\"$archFlag -Os\" -j8"
 
 make "$makeOptions" install
